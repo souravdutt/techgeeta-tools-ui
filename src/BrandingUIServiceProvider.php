9cb97625f5168a8ca0939366cfa413ef92b3035b
Sourav Dutt <sdutt12139@gmail.com>
2025-08-05 17:32:29 +0530

    auto injext ui css/js

--- src/BrandingUIServiceProvider.php
+++ src/BrandingUIServiceProvider.php
@@ -4,6 +4,8 @@ namespace TechGeeta\BrandingUI;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\AppLayout;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\File;
 
 class BrandingUIServiceProvider extends ServiceProvider
 {
@@ -29,10 +31,50 @@ class BrandingUIServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../public/assets' => public_path('/vendor/ui/assets'),
         ], 'ui-assets');
+
+
+        // Inject via Vite (auto-inclusion)
+        \Illuminate\Support\Facades\Vite::useBuildDirectory('build');
+        \Illuminate\Support\Facades\Vite::macro('techGeetaUi', function () {
+            return $this->withEntryPoints([
+                'resources/css/vendor/ui.css',
+            ]);
+        });
+
+        // Auto-inject into app.css & app.js (optional for automation)
+        $this->injectCssIntoApp();
+        $this->injectJsIntoApp();
+
     }
 
     public function register()
     {
         //
+    }
+
+    protected function injectCssIntoApp()
+    {
+        $appCss = base_path('resources/css/app.css');
+        $injectLine = "@import 'vendor/ui.css';";
+
+        if (File::exists($appCss)) {
+            $contents = File::get($appCss);
+            if (!str_contains($contents, $injectLine)) {
+                File::append($appCss, "\n".$injectLine);
+            }
+        }
+    }
+
+    protected function injectJsIntoApp()
+    {
+        $appJs = base_path('resources/js/app.js');
+        $injectLine = "import './vendor/ui.js';";
+
+        if (File::exists($appJs)) {
+            $contents = File::get($appJs);
+            if (!str_contains($contents, $injectLine)) {
+                File::append($appJs, "\n".$injectLine);
+            }
+        }
     }
 }
