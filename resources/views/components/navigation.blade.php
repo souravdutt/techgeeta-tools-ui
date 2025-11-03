ac1445bcd5d7a728243352e3be1e6acb0df2c512
Sourav Dutt <sdutt12139@gmail.com>
2025-11-03 05:57:23 +0530

    Add LinkedIn Formatter link to navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -55,6 +55,13 @@
                     Equity Splitter
                     {{-- <x-ui::badge variant="danger" class="ml-1">Beta</x-ui::badge> --}}
                 </a>
+
+                <a href="https://linkedin.techgeeta.com" class="nav-link flex gap-1 items-center {{ $domainHost === 'linkedin.techgeeta.com' ? 'active' : '' }} text-gray-700 dark:text-gray-200 hover:text-primary-600 dark:hover:text-primary-400">
+                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                    </svg>
+                    LinkedIn Formatter
+                </a>
             </div>
 
             <!-- CTA Button & Theme Switcher -->
@@ -122,6 +129,13 @@
                 </svg>
                 Equity Splitter
                 {{-- <x-ui::badge variant="danger" class="ml-auto">Beta</x-ui::badge> --}}
+            </a>
+
+            <a href="https://linkedin.techgeeta.com" class="mobile-nav-link {{ $domainHost === 'linkedin.techgeeta.com' ? 'active' : '' }} flex items-center text-gray-700 dark:text-gray-200 hover:text-primary-600 dark:hover:text-primary-400">
+                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                </svg>
+                LinkedIn Formatter
             </a>
 
             <div class="flex flex-col space-y-3 pt-3 border-t border-gray-200 dark:border-gray-700">
